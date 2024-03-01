b68f74da8d8ee00abcb287fe4514c8c888df9666
MustafaAta <65856585+AtaRekt@users.noreply.github.com>
2024-03-01 12:49:53 +0300

    Update tiktok.jsx

--- src/pages/tiktok.jsx
+++ src/pages/tiktok.jsx
@@ -129,14 +129,6 @@ export default function Tiktok() {
                 placeholder="Enter a video link..."
                 value={videoLink}
               />
-              <div onClick={paste} className="bg-white px-8 cursor-pointer flex items-center justify-center py-2 text-white rounded-lg px-2">
-                <div className="h-8 gap-2 flex items-center justify-center text-black hover:text-opacity-80 transition-all">
-                  Paste
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
-                  </svg>
-                </div>
-              </div>
               <div onClick={getVideo} className="transition-all cursor-pointer bg-opacity-80 hover:bg-opacity-100 flex items-center justify-center py-2 text-white bg-[#4E8EE5] rounded-lg px-6">
                 <div className="h-8 flex items-center justify-center">
                   {isLoading ? <img className="w-8" src={loading} alt="loading..." /> : "Download"}
